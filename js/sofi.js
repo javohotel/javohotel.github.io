15e27d2c227acf61a2f5cd68985020b4c1a583d6
Javier Ramirez <javier.ramirez@vasslatam.com>
2020-02-18 12:53:10 -0300

    viewport

--- js/sofi.js
+++ js/sofi.js
@@ -7,7 +7,7 @@ var app = new Vue({
 		      "title":"Homer the Great (1995)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BNWY4ZmNhYzEtYmU2OC00NDM2LWEzODItOGE2YTA3ODg1Y2M1XkEyXkFqcGdeQXVyNjYyODY4NDU@._V1_UY268_CR87,0,182,268_AL_.jpg",
 		      "ratingname": "uno",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/6x12-homero-el-grande",
 		      "rate":"9,0",
 		      "description":"Homer becomes a member of a mysterious organization called the Stonecutters and is heralded as 'the chosen one'."
 		   },
@@ -15,7 +15,7 @@ var app = new Vue({
 		      "title":"Homer Badman (1989)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BYzE2OWUwMDYtZTNkZC00NmIwLTkyOWMtYjcyZGVkMDFhYzI0XkEyXkFqcGdeQXVyNjQwNzY3OTQ@._V1_UY268_CR79,0,182,268_AL_.jpg",
 "ratingname": "dos",
-		      "link":"#top",
+		      "link":"#https://simpsons-latino.com/6x09-homero-el-malo",
 		      "rate":"8,9",
 		      "description":"After a series of misunderstandings, Homer is labeled a pervert, and must try to clear his name."
 		   },
@@ -23,7 +23,7 @@ var app = new Vue({
 		      "title":"Flaming Moe's (1991)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BNTIzNTI1NDI1MV5BMl5BanBnXkFtZTgwMTc3NTQ2MjE@._V1_UY209_CR69,0,140,209_AL_.jpg",
 		      "ratingname": "tres",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/3x10-llamarada-moe",
 		      "rate":"8,8",
 		      "description":"Moe claims a drink that Homer invented is his own, with the drink making the bar a local hot spot, but threatens his friendship with Homer."
 		   },
@@ -31,7 +31,7 @@ var app = new Vue({
 		      "title":"Last Exit to Springfield (1993)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BNWRiNjI0OTMtZTk0MS00NmIyLThjOGQtOGZjYTk1OTkyYTZkXkEyXkFqcGdeQXVyNjYyODY4NDU@._V1_UY209_CR69,0,140,209_AL_.jpg",
 		      "ratingname": "cuatro",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/4x17-la-ultima-salida-a-springfield",
 		      "rate":"9,1",
 		      "description":"Homer becomes union leader and leads a company strike when Mr. Burns takes away the company dental plan."
 		   },
@@ -39,7 +39,7 @@ var app = new Vue({
 		      "title":"Homer the Heretic (1989)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BZDUxMTFjMzMtYjdkZS00Mjk0LWJkZTUtMjFmYjdkZDdhYzBmXkEyXkFqcGdeQXVyNjQwMTkxMDU@._V1_UY268_CR147,0,182,268_AL_.jpg",
 		      "ratingname": "cinco",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/4x03-homero-hereje",
 		      "rate":"9,0",
 		      "description":"After skipping church one week, Homer decides that he is never going back. However, when a calamity occurs, it takes the combined efforts of a number of people from different faiths to save him."
 		   },
@@ -47,7 +47,7 @@ var app = new Vue({
 		      "title":"Treehouse of Horror V (1994)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BMjA4Nzc0NzkzMF5BMl5BanBnXkFtZTgwMTQxOTUwNDI@._V1_UY209_CR116,0,140,209_AL_.jpg",
 		      "ratingname": "seis",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/6x06-especial-de-noche-de-brujas-de-los-simpson-v",
 		      "rate":"9,1",
 		      "description":"The family's job at Mr. Burns' country estate goes awry when Homer goes mad; Homer's attempt to repair a toaster results in inadvertent time travel; The school staff turn cannibalistic."
 		   },
@@ -55,7 +55,7 @@ var app = new Vue({
 		      "title":"King Size Homer (1995)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BMjIyODQ4MjM3NF5BMl5BanBnXkFtZTgwNTk5NjA1MjE@._V1_UY209_CR14,0,140,209_AL_.jpg",
 		      "ratingname": "siete",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/7x07-homero-tamano-familiar",
 		      "rate":"9,0",
 		      "description":"Homer intentionally gains more weight in order to qualify for disability and work from home, but soon realizes that being morbidly obese comes with its own problems."
 		   },
@@ -63,7 +63,7 @@ var app = new Vue({
 		      "title":"Itchy & Scratchy Land (1994)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BZWRkY2VlZjAtNGI2My00ODBjLWJkODMtMTkwZGNkNmIzZDhmXkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
 		      "ratingname": "ocho",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/6x04-la-tierra-de-tomy-y-daly",
 		      "rate":"8.6",
 		      "description":"A family vacation to Itchy & Scratchy Land turns disastrous when the robots malfunction and turn on the tourists."
 		   },
@@ -71,7 +71,7 @@ var app = new Vue({
 		      "title":"The City of New York vs. Homer Simpson (1997)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BMTc4MzEzNjI5M15BMl5BanBnXkFtZTgwNjk5NjA1MjE@._V1_UY209_CR8,0,140,209_AL_.jpg",
 		      "ratingname": "nueve",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/9x01-la-ciudad-de-nueva-york-contra-homero",
 		      "rate":"9,1",
 		      "description":"Homer must travel to New York to get his car back, which is illegally parked at World Trade Center Plaza."
 		   },
@@ -79,7 +79,7 @@ var app = new Vue({
 		      "title":"The Wizard of Evergreen Terrace (1998)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BZDI3YTU4Y2UtMDg0ZC00YWNjLTk3MDItNzg1YzNlNTczMGE2XkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_UY209_CR116,0,140,209_AL_.jpg",
 		      "ratingname": "diez",
-		      "link":"#top",
+		      "link":"https://verlossimpsons.com/watch/los-simpsons-10x02-el-mago-de-la-terraza-siempreverde_o1O4aFx8nmZGphy.html",
 		      "rate":"8,2",
 		      "description":"Homer's midlife crisis leads to a disastrous attempt at being an inventor."
 		   },
@@ -87,7 +87,7 @@ var app = new Vue({
 		      "title":"Mayored to the Mob (1998)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BMGM4ODQxZTAtNWY3Ni00ZDQ4LThjM2EtNDE4MWFiNmJhNzBmXkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_UY209_CR71,0,140,209_AL_.jpg",
 		      "ratingname": "once",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/10x09-encuentro-con-la-mafia",
 		      "rate":"8,2",
 		      "description":"Homer deals with corruption when he becomes Mayor Quimby's bodyguard."
 		   },
@@ -95,7 +95,7 @@ var app = new Vue({
 		      "title":"Behind the Laughter (2000)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BZDk4Yjk0ZjMtNTAxZS00OTZmLTk2MmItODg0NzdkMmQ5MzIwXkEyXkFqcGdeQXVyNzA5NTYxMDg@._V1_UY209_CR17,0,140,209_AL_.jpg",
 		      "ratingname": "doce",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/11x22-detras-de-la-risa",
 		      "rate":"7,9",
 		      "description":"Fans are invited to take a behind-the-scenes look at 'The Simpsons'2, revisiting some of the most memorable experiences the family has been through."
 		   },
@@ -103,7 +103,7 @@ var app = new Vue({
 		      "title":"Marge vs. the Monorail (1993)",
 		      "img": "https://m.media-amazon.com/images/M/MV5BZmMyNzgwNDYtN2YzMS00MmViLTgwYWEtOTllYzU0Mzk3OGNjXkEyXkFqcGdeQXVyNjQwMTkxMDU@._V1_UY268_CR147,0,182,268_AL_.jpg",
 		      "ratingname": "trece",
-		      "link":"#top",
+		      "link":"https://simpsons-latino.com/4x12-marge-contra-el-monorriel",
 		      "rate":"9,1",
 		      "description":"After receiving a considerable donation of money, Springfield builds a monorail system with Homer as the conductor, unaware they've just boarded a one-way train to Hell."
 		   }
